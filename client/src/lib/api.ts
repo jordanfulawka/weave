26df7362ce01610c8c9c9df5c7a4ca6a3e0a2f70
jordanfulawka <98784255+jordanfulawka@users.noreply.github.com>
2026-06-06 15:44:04 -0400

    feat: implement register and login frontend api calls

---
+++ client/src/lib/api.ts
@@ -0,0 +1,29 @@
+async function login(email: string, password: string) {
+  const response = await fetch(
+    `${import.meta.env.VITE_API_URL}/api/auth/login`,
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ email, password }),
+    },
+  );
+  return response.json();
+}
+
+async function register(email: string, username: string, password: string) {
+  const response = await fetch(
+    `${import.meta.env.VITE_API_URL}/api/auth/register`,
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ email, username, password }),
+    },
+  );
+  return response.json();
+}
+
+export { login, register };
